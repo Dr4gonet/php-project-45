dd1481372828f9dfbdf74f2238d4ae5f83e14c31
Dr4gonet <dr4gonet@yandex.ru>
2023-03-04 20:12:32 +0300

    Changed array to random number

--- src/Even.php
+++ src/Even.php
@@ -7,23 +7,24 @@ use function cli\prompt;
 
 function even()
 {
-    $number = [15, 6, 7];
+    $number = rand(1, 20);
     $correctanswer = '';
 
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    foreach ($number as $item) {
-        if (($item % 2) === 0) {
+    for ($i = 0; $i < 3; $i += 1) {
+        if (($number % 2) === 0) {
             $correctanswer = 'yes';
         } else {
             $correctanswer = 'no';
         }
-        line("Question, %d", $item);
+        line("Question, %d", $number);
         $answer = prompt('Your answer');
         if ($answer === $correctanswer) {
             line('Correct!');
+            $number = rand(1, 20);
         } else {
             line("'%s' is wrong answer ;(. Correct answer was 'no'.", $answer);
             return line("Let's try again, %s!", $name);
