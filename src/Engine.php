658fc606f979a61b53eec28186bf5fa9d5d80a17
Dr4gonet <dr4gonet@yandex.ru>
2023-03-08 20:44:32 +0300

    general logic moved to file Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -10,4 +10,41 @@ function welcomeUser()
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
+}
+
+function frame($task, $array)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($task);
+    for ($i = 0; $i < 3; $i += 1) {
+        $count = rand(1, 3);
+        switch ($count) {
+            case 1:
+                $expression = $array[0][0];
+                $correctAnswer = (string) $array[0][1];
+                break;
+            case 2:
+                $expression = $array[1][0];
+                $correctAnswer = (string) $array[1][1];
+                break;
+            case 3:
+                $expression = $array[2][0];
+                $correctAnswer = (string) $array[2][1];
+                break;
+        }
+        line("Question, %s", $expression);
+        $answer = prompt('Your answer');
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            return line("Let's try again, %s!", $name);
+        }
+    }
+    if ($answer === $correctAnswer) {
+        line("Congratulations, %s!", $name);
+    }
 }
--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,45 +2,39 @@
 
 namespace BrainGames\Games\Calc;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Engine\frame;
 
 function calc()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('What is the result of the expression?');
-    for ($i = 0; $i < 3; $i += 1) {
-        $number1 = rand(1, 25);
-        $number2 = rand(1, 20);
-        $count = rand(1, 3);
 
-        switch ($count) {
-            case 1:
-                $expression = $number1 . ' + ' . $number2;
-                $correctAnswer = (string) ($number1 + $number2);
-                break;
-            case 2:
-                $expression = $number1 . ' - ' . $number2;
-                $correctAnswer = (string) ($number1 - $number2);
-                break;
-            case 3:
-                $expression = $number1 . ' * ' . $number2;
-                $correctAnswer = (string) ($number1 * $number2);
-                break;
-        }
+    $task = 'What is the result of the expression?';
 
-        line("Question, %s", $expression);
-        $answer = prompt('Your answer');
-        if ($answer === $correctAnswer) {
-            line('Correct!');
-        } else {
-            line("'%d' is wrong answer ;(. Correct answer was '%d'.", $answer, $correctAnswer);
-            return line("Let's try again, %s!", $name);
+    function arraytest(): array
+    {
+        $result = [];
+        for ($i = 0; $i < 3; $i += 1) {
+            $number1 = rand(1, 25);
+            $number2 = rand(1, 20);
+            $count = rand(1, 3);
+            switch ($count) {
+                case 1:
+                        $expression = $number1 . ' + ' . $number2;
+                        $correctAnswer = $number1 + $number2;
+                    break;
+                case 2:
+                        $expression = $number1 . ' - ' . $number2;
+                        $correctAnswer = $number1 - $number2;
+                    break;
+                case 3:
+                    $expression = $number1 . ' * ' . $number2;
+                    $correctAnswer = $number1 * $number2;
+            }
+            $result[$i] = [$expression, $correctAnswer];
         }
+        return $result;
     }
-    if ($answer === $correctAnswer) {
-        line("Congratulations, %s!", $name);
-    }
+
+    $array = arraytest();
+
+    frame($task, $array);
 }
