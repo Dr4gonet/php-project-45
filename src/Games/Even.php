693ef00e7a1419999f74abcd8ff7c7302fc86deb
Dr4gonet <dr4gonet@yandex.ru>
2023-03-11 00:33:01 +0300

    code corrected Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -14,15 +14,15 @@ function even()
         $result = [];
         $correctAnswer = '';
         for ($i = 0; $i < 3; $i += 1) {
-            $number = rand(1, 20); 
+            $number = rand(1, 20);
             if (($number % 2) === 0) {
                 $correctAnswer = 'yes';
             } else {
                 $correctAnswer = 'no';
             }
                 $result[] = [$number, $correctAnswer];
-            }
-        $result[$i] = [$number, $correctAnswer];
+        }
+
         return $result;
     }
 
