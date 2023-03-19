a47aa22a579c27ff862b7f8ce9d7ffcd248859c3
Dr4gonet <dr4gonet@yandex.ru>
2023-03-19 16:40:19 +0300

    checked by linter

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,31 +4,29 @@ namespace BrainGames\Games\Calc;
 
 use function BrainGames\Engine\runGame;
 
-
-
-function variationOperation(int $number1, int $number2) : array
-{  
+function variationOperation(int $number1, int $number2): array
+{
     $variationOperation = [];
     $operationOption = rand(1, 3);
     $expression = '';
     $correctAnswer = 0;
-    
+
     switch ($operationOption) {
-           case 1:
-              $expression = $number1 . ' + ' . $number2;
-              $correctAnswer = $number1 + $number2;
-              $variationOperation = [$expression, $correctAnswer];
-              break;
-          case 2:
-              $expression = $number1 . ' - ' . $number2;
-              $correctAnswer = $number1 - $number2;
-              $variationOperation = [$expression, $correctAnswer];
-             break;
-          case 3:
-              $expression = $number1 . ' * ' . $number2;
-              $correctAnswer = $number1 * $number2; 
-              $variationOperation = [$expression, $correctAnswer];
-}
+        case 1:
+            $expression = $number1 . ' + ' . $number2;
+            $correctAnswer = $number1 + $number2;
+            $variationOperation = [$expression, $correctAnswer];
+            break;
+        case 2:
+            $expression = $number1 . ' - ' . $number2;
+            $correctAnswer = $number1 - $number2;
+            $variationOperation = [$expression, $correctAnswer];
+            break;
+        case 3:
+            $expression = $number1 . ' * ' . $number2;
+            $correctAnswer = $number1 * $number2;
+            $variationOperation = [$expression, $correctAnswer];
+    }
             return $variationOperation;
 }
 
@@ -40,7 +38,6 @@ function getVariants(): array
         $number1 = rand(1, 25);
         $number2 = rand(1, 20);
         $variations[$i] = variationOperation($number1, $number2);
-
     }
         return $variations;
 }
